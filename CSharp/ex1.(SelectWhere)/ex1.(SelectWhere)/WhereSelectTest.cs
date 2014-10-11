d5af61414f60842a024ffdc1f8bcc114cfe6d376
AirVan21 <airvan21@gmail.com>
2014-10-11 14:38:57 +0400

    First code on C#

---
+++ CSharp/ex1.(SelectWhere)/ex1.(SelectWhere)/WhereSelectTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace ex1.MapFilterAnalog
+{
+    // First C# Code!
+    class SelectWhereTest
+    {
+        /*
+         * Gets last digits (except 0) from every number in inputArray via Select() 
+         * 
+         * @ inputArray - array of positive integer
+         * @ result     - array of last digits
+         */ 
+        public static int[] nonZeroLastDigits(int[] inputArray)
+        {
+            int basis = 10;
+            return inputArray.Select(number => (number % basis)).
+                Where(number => (number % basis) != 0).ToArray();
+        }
+    
+        static void Main(string[] args)
+        {
+            // Test values
+            int [] numbersFromLostFilm = {4, 8, 15, 16, 23, 42, 60};
+            Console.Write("Input numbers:  ");
+            Console.WriteLine(string.Join(", ", numbersFromLostFilm));
+            // Perfrom Select
+            int[] lastDigits = SelectWhereTest.nonZeroLastDigits(numbersFromLostFilm);
+            Console.Write("Output numbers: ");
+            Console.WriteLine(string.Join(", ", lastDigits));
+        }
+    }
+}
