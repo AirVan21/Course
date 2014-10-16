3552d81514acbba6f554d336905d4dc336e6f3aa
AirVan21 <airvan21@gmail.com>
2014-10-17 00:50:22 +0400

    Last couple of queries

--- SQL/Query/JoinBasic.sql
+++ SQL/Query/JoinBasic.sql
@@ -5,6 +5,7 @@
 /* 
 	Select Name, Surname, Phone Numbers of Employees who work in 'Machine Learning' Department
 */
+
 SELECT A2_Employee.First_Name, A2_Employee.Second_Name, A2_Employee.Phone_Number FROM A2_Contract 
 							                                                         JOIN A2_Employee ON A2_Contract.Employee_ID = A2_Employee.Employee_ID
 																				     JOIN A2_Department ON A2_Contract.Department_ID = A2_Department.Department_ID
@@ -12,6 +13,7 @@ SELECT A2_Employee.First_Name, A2_Employee.Second_Name, A2_Employee.Phone_Number
 /*
 	Shows different Position Name (all existing, because RIGHT JOIN) and shows Salary (NOT NULL if there is somebody working on this position)
 */
+
 SELECT DISTINCT A2_Position.Position_Name, A2_Post_List.Salary FROM A2_Post_List 
 																	RIGHT JOIN A2_Position ON A2_Post_List.Position_ID = A2_Position.Position_ID
 																		ORDER BY A2_Post_List.Salary DESC
@@ -20,8 +22,24 @@ SELECT DISTINCT A2_Position.Position_Name, A2_Post_List.Salary FROM A2_Post_List
 	Find out amount of Empoloyees, who are Testers AND where born After '1970-01-01'
 */
 
-SELECT COUNT (A2_Employee.Employee_ID) FROM A2_Contract
+SELECT COUNT(A2_Employee.Employee_ID) FROM A2_Contract
 											JOIN A2_Employee ON A2_Contract.Employee_ID = A2_Employee.Employee_ID
 											JOIN A2_Position ON A2_Contract.Position_ID = A2_Position.Position_ID
 												WHERE A2_Position.Position_Name LIKE '%Tester' 
-													AND A2_Employee.Birth_Date > '1970-01-01' 
+													AND A2_Employee.Birth_Date > '1970-01-01'
+/*
+	Find out amount of Employees in every Department
+*/
+
+SELECT A2_Department.Department_Name, COUNT(A2_Contract.Employee_ID) FROM A2_Contract
+																	      	WHERE A2_Contract.Department_ID = A2_Department.Department_ID
+																	      	GROUP BY Department_ID 
+
+/*
+	Show for all permanent Employees their Name, Surname and Department e-mail 
+*/
+SELECT A2_Employee.First_Name, A2_Employee.Second_Name, A2_Department.Department_Email FROM A2_Contract, A2_Employee, A2_Department
+																					  		WHERE A2_Contract.Employee_ID = A2_Employee.Employee_ID
+																					  			AND   A2_Contract.Department_ID = A2_Department.Department_ID
+																					  			AND A2_Contract.IsTemporary = 0
+																					  		
