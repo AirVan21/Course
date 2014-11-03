c03758e4c058446a6d5c068f7c62b3910582935c
AirVan21 <airvan21@gmail.com>
2014-11-03 23:14:33 +0300

    Last query is added

--- SQL/Query/GroupByBasic.sql
+++ SQL/Query/GroupByBasic.sql
@@ -3,6 +3,19 @@
 ----------------------------------
 
 /*
+	Finds how many Employees are working on specific position
+	(Ordering by most popular position)
+
+	- ORDER BY is used
+*/
+
+SELECT A2_Position.Position_Name, COUNT(*) AS 'Amount'  FROM A2_Contract
+										   JOIN A2_Employee ON A2_Contract.Employee_ID = A2_Employee.Employee_ID
+										   JOIN A2_Position ON A2_Contract.Position_ID = A2_Position.Position_ID
+												GROUP BY A2_Position.Position_Name
+													ORDER BY Amount DESC
+
+/*
 	Finds amount of Working Contracts assigned to specific Department
 
 	- JOIN is used
@@ -38,5 +51,6 @@ SELECT A2_Employee.First_Name, A2_Employee.Second_Name, A2_Employee.Birth_Date F
 SELECT DISTINCT SUBSTRING(A2_Employee.First_Name, 1, 1) as 'Name', A2_Employee.Second_Name, SUBSTRING(A2_Employee.Phone_Number, 8, 4) as 'Phone' FROM A2_Employee
 																		JOIN A2_Contract ON A2_Employee.Employee_ID = A2_Contract.Employee_ID
 																			WHERE A2_Contract.IsTemporary = 1
+
 
 
