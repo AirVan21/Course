86f319ca7b4143341cd6bee695e883fbe0630555
AirVan21 <airvan21@gmail.com>
2014-11-22 02:24:36 +0300

    Template for Predicates

---
+++ CSharp/ex6.(allDifferent)/ex6.(allDifferent)/AllDifferent.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex6._allDifferent_
+{
+    class AllDifferent
+    {
+        
+        /*
+         *  Something goes WRONG! (in NOK test)
+         */
+        
+        
+        /*
+         *   Recursive testing:
+         *      "If array has all unique numbers or not?"
+         */
+        public static bool allDifferentSubFunc(int[] array, int from, Predicate<int> condition)
+        {
+            if (array.Length > from) {
+                int element = array[from];
+                // Creating a characteristic function via Predicate concat  
+                Predicate<int> extendCondition = t => (t == element) || condition(element);
+                bool test = condition(element);
+                return (condition(element)) ? false : allDifferentSubFunc(array, (from + 1), extendCondition);
+            } else {
+                return true;
+            }
+        }
+        /*
+         *   Implementing simple interface
+         */
+        public static bool allDifferent(int[] array)
+        {
+            return allDifferentSubFunc(array, 0, x => false);
+        }
+        static void Main(string[] args)
+        {
+            int[] okArray = { 1, 2, 3, 4, 5, 10, 11 };
+            int[] nokArray = { 2, 3, 2 };
+            
+            bool okTest = allDifferent(okArray);
+             bool nokTest = allDifferent(nokArray);
+            
+            System.Console.Write("Ok test => ");
+            System.Console.WriteLine(okTest);
+            System.Console.Write("Nok test => ");
+            System.Console.WriteLine(nokTest);
+        }
+    }
+}
