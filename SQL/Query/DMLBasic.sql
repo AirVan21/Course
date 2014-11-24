ac4c4a2c8722f6a6106a79ac5d49eb480a5092cb
AirVan21 <airvan21@gmail.com>
2014-11-25 01:15:41 +0300

    Added simple DELETE queries

--- SQL/Query/DMLBasic.sql
+++ SQL/Query/DMLBasic.sql
@@ -32,3 +32,37 @@ UPDATE A2_Employee SET Phone_Number = '89115899999'
 UPDATE A2_Contract SET End_Date = DATEADD(month, 3, A2_Contract.End_Date)
 	WHERE A2_Contract.Contract_ID IN (SELECT A2_Contract.Contract_ID FROM A2_Contract
 	 										WHERE A2_Contract.IsTemporary = 1)
+/*
+	Delete records about positions, which are not represented in Post Lists (Nobody working on these positions)
+*/
+
+DELETE FROM A2_Position
+	WHERE A2_Position.Position_ID NOT IN (SELECT DISTINCT A2_Post_List.Position_ID FROM A2_Post_List)
+
+/*
+	Cascade delete. (Deleting Employee and Cascade delete all related records in Contract Table)
+
+	Using A2_Contract table as a Main Table
+	Using A2_Employee talbe as a Dependent Table
+*/
+
+-- Dropping old Constraint --
+
+ALTER TABLE A2_Contract DROP CONSTRAINT FK_A2_Contract_Employee 
+
+-- Adding Delete Cascade Constraint --
+
+ALTER TABLE A2_Contract ADD CONSTRAINT FK_A2_Contract_Employee 
+    FOREIGN KEY (Employee_ID)
+    	REFERENCES A2_Employee(Employee_ID) ON DELETE CASCADE
+
+-- Check existance of records, which should be cascade deleted (in A2_Contract) --
+SELECT A2_Contract.Contract_ID, A2_Contract.Start_Date FROM A2_Contract
+	WHERE A2_Contract.Employee_ID = (SELECT A2_Employee.Employee_ID FROM A2_Employee
+										WHERE A2_Employee.Second_Name = 'Berdskaya')
+-- DELETE Employee --
+
+DELETE FROM A2_Employee
+	WHERE A2_Employee.Second_Name = 'Berdskaya'
+
+--	No info about A2_Contract Records in A2_Contract (checked) --
