bcb6649e63b7baa2be9ded73e2dfe07a918f6606
AirVan21 <airvan21@gmail.com>
2014-12-05 17:12:16 +0300

    CPS factorial calculation

---
+++ CSharp/ex9.(cpsFactorail)/ex9.(cpsFactorail)/cpsFact.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContinuationPassingStyle
+{
+    class CPSFact
+    {
+        /*
+         * Performing Continuation Passing Style for Factorial        
+         */
+        public static int cpsFactorial(int baseCounter, Func<int, int> passFunc)
+        {
+            Func<int, int> multOn = x => x * baseCounter;
+            Func<int, int> updateFunc = x => multOn(passFunc(x));
+            return baseCounter == 0 ? passFunc(1) :  cpsFactorial(baseCounter - 1, updateFunc);
+        }
+        
+        static void Main(string[] args)
+        {
+            // Defining test constant
+            int testNum = 5;
+            Func<int, int> defaultFunc = x => 1;
+            System.Console.Write(testNum);
+            System.Console.Write("! = ");
+            System.Console.WriteLine(cpsFactorial(testNum, defaultFunc));
+        }
+    }
+}
