4e86d5401df30c0fe07cfe0f5aeca0a380eb3f09
AirVan21 <airvan21@gmail.com>
2015-09-21 00:23:50 +0300

    Added sripts

--- bash/ex1.(08.09.15)/ex9.rename.sh
+++ bash/ex1.(08.09.15)/ex9.rename.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+start=`date +%Y-%m-%d\ %H:%M:%S -d "1 day ago"`
+end=`date +%Y-%m-%d\ %H:%M:%S -d "2 days ago"`
 
-fileNames=`find ./ -type f -ctime +1 -ctime -2 -not -iname "*.sh"`
+IFS=$'\n'
+fileNames=`find ./ -type f -newermt "$end" ! -newermt "$start" -not -iname "*.sh"`
 
 for fullName in $fileNames
 do
-    name=_${fullName##*/}  # get the part after the last slash
-    newFullName=${fullName%/*}/$name
-    mv $fullName $newFullName
+    name=_${fullName##*/} # get the part after the last slash
+    newFullName=${fullName%/*}/"$name"
+    mv "$fullName" "$newFullName"
 done
 
 
