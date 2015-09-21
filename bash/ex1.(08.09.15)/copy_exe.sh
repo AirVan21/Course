9c1025bd07d3e29b59d5109fece8c51cf81382cb
AirVan21 <airvan21@gmail.com>
2015-09-21 20:33:42 +0300

    Fixed copy script

--- bash/ex1.(08.09.15)/copy_exe.sh
+++ bash/ex1.(08.09.15)/copy_exe.sh
@@ -1,39 +1,43 @@
 #!/bin/bash
 
-matchI=0
-matchO=0
-index=0
-arr[$index]=0
-
-for i in $@
-do
-    if [[ "$i" == "-i" ]]
-    then
-        let matchI=matchI+1
-    fi
-    
-    if [[ "$i" == "-o" ]]
-    then
-        let matchO=matchO+1
-    fi
-
-    if [[ "$i" != "-i" && "$i" != "-o" ]]
-    then
-        arr[$index]=$i
-        let index=index+1
-    fi
-
-done
-
-if [[ $matchO > 0 && $matchI > 0 ]]
+if [[ "$#" == 4 ]]
 then
-    # 1. Copies whole directory
-    # 2. Finds only non-executable
-    # 3. Deletes non-executable
-    
-    cp -r ${arr[0]}* ${arr[1]}
-    find ${arr[1]} -type f -not -perm /a+x | xargs rm
+    if [[ "$1" == -i ]]
+    then
+        iExsists=true
+        srcdir=$2
+    fi
+
+    if [[ "$1" == -o ]]
+    then
+        oExsists=true
+        dstdir=$2
+    fi
+
+    if [[ "$3" == -i ]]
+    then
+        iExsists=true
+        srcdir=$4
+    fi
+
+    if [[ "$3" == -o ]]
+    then
+        oExsists=true
+        dstdir=$4
+    fi
 else
-    echo "Wrong keys!"
+    echo "Wrong amount of arguments!"
+fi
+
+if [[ "$oExsists" == true && "$iExsists" == true ]]
+then
+    IFS=$'\n'
+    fileToCopy=$(find "${srcdir}" -type f -perm /a+x)
+    for file in $fileToCopy
+    do
+        cp --parents "$file" "$dstdir"
+    done
+else
+    echo "Forgotten key!"
 fi
 
