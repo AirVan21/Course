9b2668eefb83dd608475dbd25b93100d0ca76fc6
AirVan21 <airvan21@gmail.com>
2016-02-10 23:37:37 +0300

    JB task template

---
+++ bash/Duplicates/duplicates.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+# Code uses fdupes utility 
+# 
+# sudo apt-get install fdupes
+
+if [ $1 ]
+then 
+    fdupes --recurse --size --quiet "$1" | nl -bp"\s" -s"# " | sed -r -e 's/([0-9]+)#/#\1/'
+    fdupes --recurse --quiet --summarize "$1"
+else
+    echo "Please, enter a path as a first argument!"
+fi
+
